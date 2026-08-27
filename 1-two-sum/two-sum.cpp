716b5ab259d8be8e37c91ad7b2e310af853f1c0a
Mitch Belcher <MitchBelcher@Proton.me>
2026-08-27 18:49:49 -0400

    Time: 47 ms (12.99%) | Memory: 14.2 MB (63.52%) - LeetSync

---
+++ 1-two-sum/two-sum.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int inputVectorSize = nums.size();
+        int potentialMatch = 0;
+        bool matchFound = false;
+        vector<int> matchVector;
+
+        for (int index1 = 0; index1 < inputVectorSize; index1++) {
+            for (int index2 = 0; index2 < inputVectorSize; index2++) {
+
+                // Cannot use same index twice
+                if (index1 == index2) {
+                    break;
+                }
+
+                potentialMatch = nums[index1] + nums[index2]; // Temp sum
+
+                // Temporary sum matches desired target and we haven't found a match
+                if ((potentialMatch == target) && (!matchFound)) {
+                    matchFound = true;
+                    matchVector.push_back(index1);
+                    matchVector.push_back(index2);
+                    break;
+                }
+                else {
+                    potentialMatch = 0; // Ensure temp sum value is reset after each check
+                }
+            }
+            // Found a match, no need to continue
+            if (matchFound) {
+                break;
+            }
+        }
+        return matchVector;
+    }
+};
