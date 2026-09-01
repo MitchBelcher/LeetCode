8d4ca0e553f49ef80293fe54023dcc7514fe845a
Mitch Belcher <MitchBelcher@Proton.me>
2026-09-01 15:44:03 -0400

    Time: 6 ms (15.55%) | Memory: 8.6 MB (64.83%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,6 +1,39 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
+
+        // If we are given a negative number, it will never be a palindrome
+        if (x < 0)
+        {
+            return false;
+        }
+
+        // Need to know how many digits are in the input value
+        long input_magnitude = 1;
+        while (x >= 10 * input_magnitude)
+        {
+            input_magnitude = input_magnitude * 10;
+        }
+
+        // Continue until we check all digits
+        while (x != 0)
+        {
+            int left_num = x / input_magnitude; // Find left-most number by dividing by current magnitude
+            int right_num = x % 10;             // Find right-most number by mod'ing by 10
+
+            // If these don't match at any point, the input has failed the test
+            if (left_num != right_num)
+            {
+                return false;
+            }
+
+            x = (x % input_magnitude) / 10;             // Gotta reevaluate our new "input" stripped of the left and right-most numbers each loop
+            input_magnitude = input_magnitude / 100;    // Gotta reevaluate the new magnitude of the new "input" each loop
+        }
+
+        return true;
+
+        /* -- ORIGINAL SOLUTION
         string forward_string = to_string(x);   // Turn int into string directly
         string reverse_string = "";             // Placeholder for reverse of input
 
@@ -19,5 +52,6 @@ public:
         else {
             return false;
         }
+        */
     }
 };
